0e4dece92503ed9152902ad9c45fa0a8526420a4
luis daniel <luisdanielrovira8@gmail.com>
2018-06-22 01:13:07 -0400

    deploy

---
+++ assets/js/47.4f20dbb1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[47],{551:function(n,t,e){},665:function(n,t,e){"use strict";var s=e(551);e.n(s).a},850:function(n,t,e){"use strict";e.r(t);var s={},i=(e(665),e(0)),o=Object(i.a)(s,function(){var n=this.$createElement;return(this._self._c||n)("div",{},[this._v("\n  hola soy el demo\n")])},[],!1,null,null,null);t.default=o.exports}}]);
